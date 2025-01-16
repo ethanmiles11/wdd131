3d87bf9f6898b7714396f81af316abc7813471c3
ethanmiles11 <ethan.miles.1114@gmail.com>
2025-01-16 15:55:31 -0700

    Added Event functionality to BOM exercise

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,12 +1,29 @@
-const input = document.querySelector("favchap");
+const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
-const list = document.querySelector("list");
+const list = document.querySelector("#list");
 
-const li = document.createElement("li");
-li.textContent = input.value;
+button.addEventListener("click", function(){
+    if (input.value.trim() !== "") {
+        // Create new list item
+        const li = document.createElement("li");
+        li.textContent = input.value;
 
-const deleteButton = document.createElement("button");
-deleteButton.textContent = "❌";
+        // Create delete button to go with list item
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "❌";
 
-li.append(deleteButton);
-list.append(li);
+        // Add delete functionality
+        deleteButton.addEventListener("click", function () {
+            list.removeChild(li);
+        });
+
+        //Append delete button and list item
+        li.append(deleteButton);
+        list.append(li);
+        input.focus();
+    } else {
+        input.value = "";
+        input.focus();
+    }
+});
+
